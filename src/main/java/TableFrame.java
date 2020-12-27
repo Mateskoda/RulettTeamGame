aaf5813533fc32b8fce3b8dd3b076b747ac40b7a
gabort07 <gabort07@hotmail.com>
2020-12-27 12:05:11 +0100

    Ground commit1

---
+++ src/main/java/TableFrame.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class TableFrame extends JFrame {
+
+    TableFrame() {
+
+
+
+        this.setTitle("RúlezRoulett");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(1200, 800);
+        this.setVisible(true);
+    }
+}
